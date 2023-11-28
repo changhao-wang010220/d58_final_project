69b681e7df50fd27c913f7454d239599029a85c2
Zijian Fan <jerryfan785@gmail.com>
2023-11-28 07:23:05 +0000

    the server will keep hanging and take path from client

--- client.c
+++ client.c
@@ -8,26 +8,40 @@
 #include <netinet/in.h>
 #include <sys/types.h>
 #include <arpa/inet.h>
- 
+
 #define BUF_SIZE (8192)
 #define PORT 10001
- 
+
 unsigned char fileBuf[BUF_SIZE];
- 
-void file_client(const char *ip, const char *path) {
+
+/*
+ * Function to send file path to the server
+ */
+void send_file_path(int client_socket, const char *path) {
+    if (write(client_socket, path, strlen(path) + 1) < 0) {
+        perror("write");
+        close(client_socket);
+        exit(1);
+    }
+}
+
+/*
+ * Function to receive the file from the server
+ */
+void file_client(const char *ip, const char *sendPath, const char *receivePath) {
     int client_socket;
     FILE *fp = NULL;
     struct sockaddr_in sockAddr;
     unsigned int fileSize, fileSize2;
     int size, nodeSize;
- 
-    if((client_socket=socket(AF_INET,SOCK_STREAM,0)) < 0) {
+
+    if ((client_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("socket");
         exit(1);
     } else {
         printf("socket success!\n");
     }
- 
+
     sockAddr.sin_family = AF_INET;
     sockAddr.sin_port = htons(PORT);
     // Convert IPv4 and IPv6 addresses from text to binary form
@@ -35,39 +49,42 @@ void file_client(const char *ip, const char *path) {
         perror("Invalid address or address not supported");
         exit(EXIT_FAILURE);
     }
- 
+
     // Connect to the server
     if (connect(client_socket, (struct sockaddr *)&sockAddr, sizeof(sockAddr)) == -1) {
         perror("Connection failed");
         exit(EXIT_FAILURE);
     }
- 
+
+    // Send file path to the server
+    send_file_path(client_socket, sendPath);
+
     size = read(client_socket, (unsigned char *)&fileSize, 4);
-    if( size != 4 ) {
+    if (size != 4) {
         printf("file size error!\n");
         close(client_socket);
         exit(-1);
     }
     printf("file size:%d\n", fileSize);
- 
-    if( (size = write(client_socket, "OK", 2) ) < 0 ) {
+
+    if ((size = write(client_socket, "OK", 2)) < 0) {
         perror("write");
         close(client_socket);
         exit(1);
     }
- 
-    fp = fopen(path, "w");
-    if( fp == NULL ) {
+
+    fp = fopen(receivePath, "w");
+    if (fp == NULL) {
         perror("fopen");
         close(client_socket);
         return;
     }
- 
+
     fileSize2 = 0;
-    while(memset(fileBuf, 0, sizeof(fileBuf)), (size = read(client_socket, fileBuf, sizeof(fileBuf))) > 0) {
+    while (memset(fileBuf, 0, sizeof(fileBuf)), (size = read(client_socket, fileBuf, sizeof(fileBuf))) > 0) {
         unsigned int size2 = 0;
-        while( size2 < size ) {
-            if( (nodeSize = fwrite(fileBuf + size2, 1, size - size2, fp) ) < 0 ) {
+        while (size2 < size) {
+            if ((nodeSize = fwrite(fileBuf + size2, 1, size - size2, fp)) < 0) {
                 perror("write");
                 close(client_socket);
                 exit(1);
@@ -75,21 +92,21 @@ void file_client(const char *ip, const char *path) {
             size2 += nodeSize;
         }
         fileSize2 += size;
-        if(fileSize2 >= fileSize) {
+        if (fileSize2 >= fileSize) {
             break;
         }
     }
     fclose(fp);
     close(client_socket);
 }
- 
+
 int main(int argc, char **argv) {
-    if( argc < 3 ) {
+    if (argc < 4) {
         printf("file client: argument error!\n");
         return -1;
     }
- 
-    file_client(argv[1], argv[2]);
- 
+
+    file_client(argv[1], argv[2], argv[3]);
+
     return 0;
 }
--- server.c
+++ server.c
@@ -8,116 +8,150 @@
 #include <netinet/in.h>
 #include <sys/types.h>
 #include <arpa/inet.h>
- 
-#define BUF_SIZE  (8192)
+#include <pthread.h>
+
+#define BUF_SIZE (8192)
 #define PORT 10001
- 
+
 unsigned char fileBuf[BUF_SIZE];
- 
+
 /*
- * send file
+ * Function to set up the socket
  */
-void file_server(const char *path) {
-    int skfd, cnfd;
-    FILE *fp = NULL;
-    struct sockaddr_in sockAddr, cltAddr;
-    socklen_t addrLen;
-    unsigned int fileSize;
-    int size, netSize;
-    char buf[10];
- 
-    if( !path ) {
-        printf("file server: file path error!\n");
-        return;
-    }
- 
-    if((skfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+int setup_socket() {
+    int skfd;
+    struct sockaddr_in sockAddr;
+
+    if ((skfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("socket");
         exit(1);
     } else {
         printf("socket success!\n");
     }
- 
+
     memset(&sockAddr, 0, sizeof(struct sockaddr_in));
     sockAddr.sin_family = AF_INET;
     sockAddr.sin_addr.s_addr = htonl(INADDR_ANY);
     sockAddr.sin_port = htons(PORT);
- 
-    //bind
-    if(bind(skfd, (struct sockaddr *)(&sockAddr), sizeof(struct sockaddr)) < 0) {
+
+    // bind
+    if (bind(skfd, (struct sockaddr *)(&sockAddr), sizeof(struct sockaddr)) < 0) {
         perror("Bind");
         exit(1);
     } else {
         printf("bind success!\n");
     }
- 
-    if(listen(skfd, 4) < 0) {
+
+    if (listen(skfd, 4) < 0) {
         perror("Listen");
         exit(1);
     } else {
         printf("Server listening on port %d...\n", PORT);
     }
- 
-    addrLen = sizeof(struct sockaddr_in);
-    if((cnfd = accept(skfd, (struct sockaddr *)(&cltAddr), &addrLen)) < 0) {
-        perror("Accept");
+
+    return skfd;
+}
+
+/*
+ * Function to receive file path from the client
+ */
+char *receive_file_path(int cnfd) {
+    static char path[256];  // Assuming a maximum file path length of 255 characters
+
+    if (read(cnfd, path, sizeof(path)) < 0) {
+        perror("read");
         exit(1);
-    } else {
-        printf("accept success!\n");
     }
- 
+
+    return path;
+}
+
+/*
+ * send file
+ */
+void *handle_client(void *arg) {
+    int cnfd = *((int *)arg);
+    FILE *fp = NULL;
+    unsigned int fileSize;
+    int size, netSize;
+    char buf[10];
+
+    // Receive file path from the client
+    char *path = receive_file_path(cnfd);
+    printf("Received file path: %s\n", path);
+
     fp = fopen(path, "r");
-    if( fp == NULL ) {
+    if (fp == NULL) {
         perror("fopen");
         close(cnfd);
-        close(skfd);
-        return;
+        pthread_exit(NULL);
     }
- 
+
     fseek(fp, 0, SEEK_END);
     fileSize = ftell(fp);
     fseek(fp, 0, SEEK_SET);
- 
-    if(write(cnfd, (unsigned char *)&fileSize, 4) != 4) {
+
+    if (write(cnfd, (unsigned char *)&fileSize, 4) != 4) {
         perror("write");
         close(cnfd);
-        close(skfd);
-        exit(1);
+        fclose(fp);
+        pthread_exit(NULL);
     }
- 
-    if( read(cnfd, buf, 2) != 2) {
+
+    if (read(cnfd, buf, 2) != 2) {
         perror("read");
         close(cnfd);
-        close(skfd);
-        exit(1);
+        fclose(fp);
+        pthread_exit(NULL);
     }
- 
-    while( ( size = fread(fileBuf, 1, BUF_SIZE, fp) ) > 0 ) {
+
+    while ((size = fread(fileBuf, 1, BUF_SIZE, fp)) > 0) {
         unsigned int size2 = 0;
-        while( size2 < size ) {
-            if( (netSize = write(cnfd, fileBuf + size2, size - size2) ) < 0 ) {
+        while (size2 < size) {
+            if ((netSize = write(cnfd, fileBuf + size2, size - size2)) < 0) {
                 perror("write");
                 close(cnfd);
-                close(skfd);
-                exit(1);
+                fclose(fp);
+                pthread_exit(NULL);
             }
             size2 += netSize;
         }
     }
- 
+
     fclose(fp);
     close(cnfd);
-    close(skfd);
+    pthread_exit(NULL);
 }
- 
-int main(int argc, char **argv) {
-    if( argc < 2 ) {
-        printf("file server: argument error!\n");
-        printf("file_server /tmp/temp\n");
-        return -1;
+
+int main() {
+    int skfd = setup_socket();
+
+    while (1) {
+        int cnfd;
+        struct sockaddr_in cltAddr;
+        socklen_t addrLen = sizeof(struct sockaddr_in);
+
+        // Accept a new client connection
+        if ((cnfd = accept(skfd, (struct sockaddr *)(&cltAddr), &addrLen)) < 0) {
+            perror("Accept");
+            continue;
+        } else {
+            printf("accept success!\n");
+        }
+
+        // Create a new thread to handle the client
+        pthread_t thread;
+        if (pthread_create(&thread, NULL, handle_client, &cnfd) != 0) {
+            perror("pthread_create");
+            close(cnfd);
+            continue;
+        }
+
+        // Detach the thread so it can automatically clean up resources when it finishes
+        pthread_detach(thread);
     }
- 
-    file_server(argv[1]);
- 
+
+    close(skfd);
+
     return 0;
 }
